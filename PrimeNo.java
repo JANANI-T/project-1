ab01abd8b22ede026732237265224b9181c65e3b
JANANI-T <jananisidh@gmail.com>
2017-07-04 15:13:14 +0530

    Update and rename Prime.java to PrimeNo.java

---
+++ PrimeNo.java
@@ -0,0 +1,28 @@
+# project-1
+import java.io.*;
+import java.util.*;
+public class PrimeNo
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+int n,prime=0;
+System.out.println("Enter the number:");
+n = sc.nextInt();
+if(n%2==0 && n>=2)
+{
+prime=0;
+}
+else
+{
+prime =1;
+}
+if(prime==0)
+{
+System.out.println("The given is not Prime");
+}
+else
+{
+System.out.println("The given is  Prime");
+}}
+}
