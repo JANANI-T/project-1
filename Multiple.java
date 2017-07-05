4d76d5b99078c2a3b124d905d9cc72952decbbbc
JANANI-T <jananisidh@gmail.com>
2017-07-05 09:50:39 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,14 @@
+# project-1
+import java.io.*;
+import java.util.*;
+public class Multiple{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Number:");
+int n = sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+System.out.println(n+" * "+i+" = "+n*i);
+}
+}}
